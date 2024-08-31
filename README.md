ab687b49b9a2f0edf231e514d9208a7c0f299109
Adryan <adryanarthur714@gmail.com>
2024-08-31 11:08:48 -0300

    criação do repositório e adição dos arquivos
    criei o repositório e adicionei os arquivos com os códigos

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# VerificadorDeIdade
- Exercício prospoto no curso de JavaScript do Curso em video, criar um site onde o usuario possa digitar o ano de nascimento e escolher um gênero 
+# Exercício de Verificação de Idade
+ Este projeto é um exercício proposto no curso de JavaScript do Curso em Vídeo, onde o objetivo é criar um site em que o usuário possa digitar o ano de nascimento e escolher o gênero. Usando JavaScript, o ano será calculado e retornará a idade e a faixa etária correspondente.
+
+## Aviso Importante sobre o Uso das Imagens
+  Este projeto foi desenvolvido para fins educativos e utiliza imagens sem autorização prévia. Em nenhum momento durante o curso do Curso em Vídeo foi sugerido ou 
+ recomendado o uso de imagens sem autorização. Pelo contrário, o professor Guanabara explicitamente aconselhou a não utilizar imagens sem a devida permissão.
+
+  O uso dessas imagens foi uma decisão pessoal e estou ciente de que isso não está de acordo com as melhores práticas recomendadas no curso. Se você é o detentor dos 
+ direitos dessas imagens e gostaria que elas fossem removidas, por favor, entre em contato.
+
+ Nota: Ao clonar este repositório, recomenda-se substituir as imagens por versões autorizadas ou de uso livre.
+
+## Objetivo
+ Criar um site onde o usuário possa inserir o ano de nascimento e escolher um gênero. Utilizando JavaScript, o site calculará a idade e determinará a faixa etária da pessoa, exibindo uma foto meramente ilustrativa de acordo com a idade calculada.
+
+Regras de Classificação por Faixa Etária:
+Gênero Masculino:
+
+0 a 12 anos: Menino
+13 a 18 anos: Garoto
+19 a 50 anos: Adulto
+Acima de 50 anos: Idoso
+Gênero Feminino:
+
+0 a 12 anos: Menina
+13 a 18 anos: Garota
+19 a 50 anos: Adulta
+Acima de 50 anos: Idosa
---
+++ src/instrucoes.js
@@ -0,0 +1,50 @@
+function verificar(){
+    var anoNasc = Number(document.querySelector("#anoNasc").value)
+    var generoEscolhido = document.getElementsByName("genero")
+    var anoAtual = new Date().getFullYear()
+
+    if(anoNasc <= 0 || anoNasc > anoAtual || !generoEscolhido[0].checked && !generoEscolhido[1].checked){
+        alert("[ERRO] verifique os dados inseridos e tente novamente!")
+    }
+    else{
+        var image = document.createElement("img")
+        image.id = "fotos"
+        image.alt = "Imagens de personagens conforme a idade calculada"
+        var idade = anoAtual - anoNasc
+
+        if(generoEscolhido[0].checked){
+            if(idade >= 0 && idade <= 12){ //Criança
+                fxEtaria = "menino"
+                image.src = "./images/foto-crianca-m.png"
+            }else if(idade <= 18){ //adolecente
+                fxEtaria = "garoto"
+                image.src = "./images/foto-jovem-m.png"
+            }else if(idade <= 50){ //adulto
+                fxEtaria = "adulto"
+                image.src = "./images/foto-adulto-m.png"
+            }else{ //velho
+                fxEtaria = "idosa"
+                image.src = "./images/foto-velho-m.png"
+            }
+        }
+        else if(generoEscolhido[1].checked){
+            if(idade >= 0 && idade <= 12){ //Criança
+                fxEtaria = "menina"
+                image.src = "./images/foto-crianca-f.png"
+            }else if(idade <= 18){ //adolecente
+                fxEtaria = "garota"
+                image.src = "./images/foto-jovem-f.png"
+            }else if(idade <= 50){ //adulta
+                fxEtaria = "adulta"
+                image.src = "./images/foto-adulta-f.png"
+            }else{ //velha
+                fxEtaria = "idosa"
+                image.src = "./images/foto-velha-f.png"
+            }
+        }
+        var resMsg = document.querySelector("div#res")
+        resMsg.style.textAlign = "center"
+        resMsg.innerHTML = `<p>${fxEtaria} com ${idade} anos</p>` 
+        resMsg.appendChild(image)
+    } 
+}
